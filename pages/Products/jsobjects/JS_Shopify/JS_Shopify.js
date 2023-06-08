e53738aefb965051d8764c81d7f4fade83f90309
Nick Quispel <nickquispel@vargar.eu>
2023-06-08 03:29:56 +0000

    add check if response got product, otherwise skip update shopifyid

--- pages/Products/jsobjects/JS_Shopify/JS_Shopify.js
+++ pages/Products/jsobjects/JS_Shopify/JS_Shopify.js
@@ -1,32 +1,34 @@
 export default {
-	 Create_Products: () => {
+	Create_Products: () => {
 		GetSettings.run()
-	.then(() => closeModal('Modal_Tags'))
-	.then(() => showModal('Modal_Loading'))
-	.then(() => ShopifyInsertArticle.run())
-  .then((res) => {
+			.then(() => closeModal('Modal_Tags'))
+			.then(() => showModal('Modal_Loading'))
+			.then(() => ShopifyInsertArticle.run())
+			.then((res) => {
 			res.products.forEach(function(product){
-			UpdateShopifyID.run({shopify_id: product.product.id, shopify_inv_id:product.product.variants[0].inventory_item_id, id:product.product.variants[0].sku, variant_id:product.product.variants[0].id})
+				if (product.product.id){
+					UpdateShopifyID.run({shopify_id: product.product.id, shopify_inv_id:product.product.variants[0].inventory_item_id, id:product.product.variants[0].sku, variant_id:product.product.variants[0].id})
+				}
+			})
 		})
+			.then(() => {
+			showAlert('Product inserted', 'success');
+			closeModal('Modal_Loading');
+			GetProducts.run();
 		})
-  .then(() => {
-								showAlert('Product inserted', 'success');
-								closeModal('Modal_Loading');
-								GetProducts.run();
-							})
-	.catch((err) => {
-										showAlert(`${JSON.stringify(err.message)}`,'error');
-										closeModal('Modal_Loading');
-									})
-	
-  // .catch((error) => {	;
-											// InsertErrorlog.run() })												//write code here
+			.catch((err) => {
+			showAlert(`${JSON.stringify(err.message)}`,'error');
+			closeModal('Modal_Loading');
+		})
+
+		// .catch((error) => {	;
+		// InsertErrorlog.run() })												//write code here
 	},
 	Create_Product_Variants: () => {
 		GetSettings.run()
-	.then(() => showModal('Modal_Loading'))
-	.then(() => ShopifyInsertArticleVariants.run())
-	.then((res) => {
+			.then(() => showModal('Modal_Loading'))
+			.then(() => ShopifyInsertArticleVariants.run())
+			.then((res) => {
 			if (res.error) {
 				showAlert(res.error.message,'error')
 			} else {
@@ -36,7 +38,7 @@ export default {
 				closeModal('Modal_Loading')
 			}
 		})
-		.catch((err) => showAlert(JSON.stringify(err,'error')))
+			.catch((err) => showAlert(JSON.stringify(err,'error')))
 	},
 	getSettings: () => {
 		GetSettings.run()
